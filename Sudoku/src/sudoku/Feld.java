87173f08577c60c891e95dbf622a09059c34b0dd
wafab112 <wafab112@gmail.com>
2019-01-20 11:32:28 +0100

    b

--- Sudoku/src/sudoku/Feld.java
+++ Sudoku/src/sudoku/Feld.java
@@ -175,11 +175,11 @@ public class Feld extends JFrame {
 		
 		String path = System.getProperty("user.dir");
 		
+		System.out.println(path + "/Resource/IndieFlower.ttf");
+		
 		try {
 			
 		    font = Font.createFont(Font.TRUETYPE_FONT, new File(path + "/Resource/IndieFlower.ttf")).deriveFont(12f);
-		    
-		    System.out.println(path + "/Resource/IndieFlower.ttf");
 		    
 		} catch (IOException e) {
 			
